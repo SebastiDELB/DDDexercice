2689394079a5af7e4da62dd9ea97d10219d269bd
SebastiDELB <sebas9241@hotmail.fr>
2020-04-22 14:50:24 +0200

    valueobject + créaneau

---
+++ DDDexercice/DDDexercice/Creneau.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDexercice
+{
+    public class Creneau : ValueObject
+    {
+        public DateTime date { get; private set; }
+        public int heureDebut { get; private set; }
+        public int heureFin { get; private set; }
+
+        private Creneau() { }
+
+        public Creneau(DateTime dateCreneau, int duree)
+        {
+            date = dateCreneau;
+            heureDebut = dateCreneau.Hour;
+            heureFin = heureDebut + duree;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return date;
+            yield return heureDebut;
+            yield return heureFin;
+        }
+    }
+}
