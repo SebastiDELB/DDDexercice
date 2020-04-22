a9a326f38fc0c2a20db98b9a780509f1936fd942
SebastiDELB <sebas9241@hotmail.fr>
2020-04-22 16:41:37 +0200

    entretien

---
+++ DDDexercice/DDDexercice/Entretien.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDexercice
+{
+    class Entretien
+    {
+        private Guid entretienid;
+        private string status;
+        private Creneau creneau;
+        private string recruteur;
+        private string candidat;
+
+        Entretien(Creneau unCreneau, string unRecruteur, string unCandidat)
+        {
+            entretienid = new Guid();
+            status = "en attente";
+            creneau = unCreneau;
+            recruteur = unRecruteur;
+            candidat = unCandidat;
+        }
+        public void Confirmer()
+        {
+            status = "accepté";
+        }
+        public void Annuler(string raison)
+        {
+            status = "annulé " + raison; 
+        }
+    }
+}
